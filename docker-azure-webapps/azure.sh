6365b3eb97be468ee51d429f220d5e34142505de
mogamin2 <takashi.mogami@ulsystems.co.jp>
2024-06-28 21:58:08 +0000

    added

---
+++ docker-azure-webapps/azure.sh
@@ -0,0 +1,10 @@
+az webapp create --resource-group mogamin-appservice-multicontainer-test-group --plan mogaminAppServicePlan --name mogaminDify --multicontainer-config-type compose --multicontainer-config-file docker-compose.yaml
+
+az webapp config appsettings set --resource-group mogamin-appservice-multicontainer-test-group --name mogaminDify --settings WEBSITES_ENABLE_APP_SERVICE_STORAGE=TRUE
+
+az webapp config container set --resource-group mogamin-appservice-multicontainer-test-group --name mogaminDify --multicontainer-config-type compose --multicontainer-config-file docker-compose.yaml
+
+
+
+
+
